c8754f2bf003234b57fb7087a11f7c1a75f3ef29
hyeopsang <dbtkdguq3@gmail.com>
2025-04-10 18:56:26 +0900

    fix:firebase 수정

--- firebase.ts
+++ firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  databaseURL: "https://allday0705-default-rtdb.firebaseio.com"
+  databaseURL: "https://song-df24e-default-rtdb.firebaseio.com"
 };
 
 export const app = initializeApp(firebaseConfig);
